d1e59b4efb08beda52986a67f9c7c17f0a7e2b55
BrightBotTeam <anoffcialemail@gmail.com>
2015-12-09 23:21:16 -0500

    Commitment is hard.

---
+++ DeisticPixelDungeon/src/com/avmoga/dpixel/sprites/RaceArtifactSpriteSheet.java
@@ -0,0 +1,10 @@
+package com.avmoga.dpixel.sprites;
+
+public class RaceArtifactSpriteSheet {
+	private static final int ROW1 = 0 * 16;
+	
+	public static final int NULLWARN = ROW1 + 0;
+	public static final int DEWDROP = ROW1 + 1;
+	public static final int PETAL = ROW1 + 2;
+	public static final int SANDBAG = ROW1 + 3;
+}
